dfa4c90c7a3e8420c4f5ccca2407223daf91e20e
Sadiq Shah <52124323+sadiqshah786@users.noreply.github.com>
2024-07-31 17:47:51 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import Image from "next/image";
+import React from "react";
+
+// const navigation = [
+//   { name: "Projects", href: "/projects" },
+//   { name: "Contact", href: "/contact" },
+// ];
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
+      <div className="">
+        <Image src="/logo.png" alt="logo" width={400} height={400}/>
+      </div>
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <h1 className="py-3.5 px-0.5 z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        Saltex Pvt Ltd
+      </h1>
+
+      <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
+      <div className="my-16 text-center animate-fade-in">
+        <h2 className="text-lg text-zinc-500 ">
+         Website Under Development
+        </h2>
+      </div>
+    </div>
+  );
+
+}
---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import { withContentlayer } from "next-contentlayer";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	experimental: {
+		mdxRs: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	output:"export",
+};
+
+export default withContentlayer(nextConfig);
